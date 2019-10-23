e6ebfa3fddd3edf41e92e5d557555b2537873536
Bazch <doppleb@gmail.com>
2019-10-23 13:19:07 +0200

    Doelstelling 1: Kaarten laten zien en shufflen  (23-10-2019)

--- src/Cards.java
+++ src/Cards.java
@@ -1,10 +1,12 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Cards {
 
     private Suits suit;
     private Ranks rank;
+
+    static ArrayList<Cards> deck1 = new ArrayList<Cards>();
 
     public Cards(Suits suit, Ranks rank) {
         this.suit = suit;
@@ -25,9 +27,18 @@ public class Cards {
     public static void main(String[] args) {
         for(Suits s : Suits.values()){
            for(Ranks r : Ranks.values()){
-               Cards c = new Cards(s,r);
-               System.out.println(c.suit + " " + c.rank);
+               deck1.add(new Cards(s,r));
            }
+        }
+        System.out.println("Sorted deck: ");
+        for(int i=0; i < deck1.size(); i++) {
+            System.out.print(deck1.get(i).suit + " " + deck1.get(i).rank + "  ");
+        }
+
+        Collections.shuffle(deck1);
+        System.out.println("\n \nShuffled deck: ");
+        for(int i=0; i < deck1.size(); i++) {
+            System.out.print(deck1.get(i).suit + " " + deck1.get(i).rank + "  ");
         }
     }
 }
